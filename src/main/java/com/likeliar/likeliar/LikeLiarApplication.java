d321266773da50c8edc3d563959adda1966c3665
giwoong <dev.choi0409@gmail.com>
2024-07-17 22:14:48 +0900

    first setting

---
+++ src/main/java/com/likeliar/likeliar/LikeLiarApplication.java
@@ -0,0 +1,13 @@
+package com.likeliar.likeliar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LikeLiarApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LikeLiarApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/likeliar/likeliar/LikeLiarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.likeliar.likeliar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LikeLiarApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
