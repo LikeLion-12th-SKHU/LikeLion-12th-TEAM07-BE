ec134e061d2ca2aa31cc173905ec44c5bffe2f48
giwoong <dev.choi0409@gmail.com>
2024-08-02 01:32:04 +0900

    Test: Image 테스트

---
+++ src/main/java/com/likeliar/likeliar/image/application/ImageService.java
@@ -0,0 +1,18 @@
+package com.likeliar.likeliar.image.application;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+public class ImageService {
+
+    @Value("${spring.cloud.gcp.storage.credentials.location}")
+    private String keyFileName;
+
+    @Value("${spring.cloud.gcp.storage.bucket}")
+    private String bucketName;
+
+
+}
